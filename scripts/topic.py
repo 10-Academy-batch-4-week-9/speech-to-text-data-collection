6cc32b61e1d8cfd059377b5087d97b0e27185419
ZANOU Christian <zchristian955@gmail.com>
2021-09-10 10:54:39 +0200

    'speech'

---
+++ scripts/topic.py
@@ -0,0 +1,10 @@
+from kafka import KafkaConsumer
+from kafka.admin import KafkaAdminClient, NewTopic
+admin_client = KafkaAdminClient(bootstrap_servers="b-1.demo-cluster-1.9q7lp7.c1.kafka.eu-west-1.amazonaws.com:9092", client_id='group4')
+
+topic_list = []
+# topic_list.append(NewTopic(name="group4_test", num_partitions=1, replication_factor=2))
+topic_list.append(NewTopic(name="msgpack-topic", num_partitions=1, replication_factor=2))
+topic_list.append(NewTopic(name="json-topic", num_partitions=1, replication_factor=2))
+admin_client.create_topics(new_topics=topic_list, validate_only=False)
+
