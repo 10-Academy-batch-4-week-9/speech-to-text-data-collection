6ad40346d22bc498c09c864c341f0a76fa449401
Desmond Onam <desmondonam@gmail.com>
2021-09-11 20:04:16 +0300

    add Javascript tag

---
+++ consumer/index.js
@@ -0,0 +1,18 @@
+import Kafka from 'node-rdkafka';
+import eventType from '../eventType.js';
+
+var consumer = new Kafka.KafkaConsumer({
+  'group.id': 'kafka',
+  'metadata.broker.list': 'localhost:9092',
+}, {});
+
+consumer.connect();
+
+consumer.on('ready', () => {
+  console.log('consumer ready..')
+  consumer.subscribe(['test']);
+  consumer.consume();
+}).on('data', function(data) {
+  console.log(`received message: ${eventType.fromBuffer(data.value)}`);
+});
+© 2021 GitHub, Inc.
