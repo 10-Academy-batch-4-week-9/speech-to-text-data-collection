a69047391fecaef69619ec9bc29565522790f510
Bereket Kibru <bekakibru2@gmail.com>
2021-09-13 14:39:53 +0300

    update the frontend

--- server/main.py
+++ server/main.py
@@ -23,21 +23,6 @@ def index():
         print('file uploaded successfully')
 
         return render_template('index.html', request="POST")
-@app.route("/app2", methods=['POST', 'GET'])
-def app2():
-    if request.method == 'GET':
-        # randomly select text and send to user
-        text =  "የተሻለ ብቃት ያሳዩ ቦክሰኞች ለቶኪዮ ኦሊምፒክ ማጣሪያ ተሳታፊ እንደሚሆኑም ታውቋል"
-        return render_template('index2.html',data=text)
-    if request.method == "POST":
-        f = request.files['audio_data']
-        with open('audio.wav', 'wb') as audio:
-            #send the file to kafka
-            f.save(audio)
-        print('file uploaded successfully')
-
-        return render_template('index2.html', request="POST")
-
 
 
 if __name__ == "__main__":
